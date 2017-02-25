a42b8d97cd3930a43d090a93a647583cbbacc79b
PatrycjaPrz <patrycja.kamila.przybylowicz@gmail.com>
2017-02-25 14:52:26 +0100

    First version of application - added first view (it is only first concept - it will be modified), but the application still does nothing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FoodBook
+
+This is a simple Spring Boot based web application allowing users to create their collection of recipies and food photos and to interact with other users.
---
+++ src/main/java/com/foodbook/FoodBookApplication.java
@@ -0,0 +1,19 @@
+package com.foodbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FoodBookApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FoodBookApplication.class);
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBookApplication.class, args);
+	}
+}
